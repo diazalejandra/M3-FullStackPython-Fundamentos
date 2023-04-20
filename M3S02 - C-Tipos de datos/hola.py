fcffaad868c263f7fda94d341439034f143e6a85
alejandra.diaz <alejandra.diaz@ixcomercio.com>
2023-04-19 22:01:26 -0400

    Tipos de dato en python

---
+++ M3S02 - C-Tipos de datos/hola.py
@@ -0,0 +1,21 @@
+print("Hola Mundo")
+print("Esto es una línea nueva")
+
+edad = 50
+sueldo = 1000.50
+nombre = "Ale"
+isTeacher = False
+
+print(edad, sueldo, nombre, isTeacher)
+
+numero_1 = 1
+numero_2 = 2
+numero_3 = 3
+
+resultado = (((numero_1*numero_2)+100)/numero_3)
+print(resultado)
+
+potencia = 15**3
+sumandos = potencia + numero_3
+
+print(sumandos)
